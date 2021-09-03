293ef2a07caf5bc4e8f734a5c4959837b5382c62
Spencer Ward <spencerwardaudio@gmail.com>
2021-09-03 20:44:40 +0200

    Added container handling methods

--- Source/CircularBuffer.h
+++ Source/CircularBuffer.h
@@ -13,13 +13,60 @@
 template<typename T>
 struct CircularBuffer
 {
-    using DataType = ... //some container holding T's
+    using DataType = std::vector<T>; //some container holding T's
     
-    void resize(std::size_t s, T fillValue);
-    void clear(T fillValue);
-    void write(T t);
-    DataType& getData();   
-    size_t getReadIndex() const;
-    size_t getSize() const;
+    void resize(std::size_t s, T fillValue)
+    {
+        elements.resize(s);
+        
+        clear(fillValue);
+    }
+    
+    void clear(T fillValue)
+    {
+        //reset elements in the buffer to initial Value
+        std::fill(elements.begin(), elements.end(), fillValue);
+    }
+    
+    void write(T t)
+    {
+        //write a value to write pointer index
+        elements[writeIndex]->t;
+        
+        writeIndex++;
+        
+        if(writeIndex == getSize())
+        {
+            writeIndex = 0;
+        }
+    }
+    
+    DataType& getData()
+    {
+        return elements;
+    }
+    
+    size_t getReadIndex() const
+    {
+        readIndex = writeIndex + 1;
+        
+        if(readIndex == getSize())
+        {
+            readIndex = 0;
+        }
+        
+        return readIndex;
+    }
+    
+    size_t getSize() const
+    {
+        return elements.size();
+    }
+    
 private:
+    
+    std::vector<T> elements { };
+    
+    size_t writeIndex {};
+    size_t readIndex {};
 };
